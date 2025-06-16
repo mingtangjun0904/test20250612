c6e3c07f59a7d45e08d63c77cf14c9ba0f0bb800
tangjun <tangjun030904@163.com>
2025-06-17 06:54:58 +0800

    优化了js里向指定ID元素写内容的步骤

--- test1.js
+++ test1.js
@@ -1,6 +1,10 @@
 function myFunction(){
-     x=document.getElementById("demo");//获取ID
-     x.innerHTML="已经改变";//写入内容
+
+     // x=document.getElementById("demo");//获取ID
+     // x.innerHTML="已经改变";//写入内容
+     document.getElementById("demo").innerHTML="这是javaScript函数"
+     console.log("1")
+
 }
 
 
