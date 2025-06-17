2a0fcfa351ff73fb19f69c4ab8c187e5db4607b3
TangJun <3040045453@qq.com>
2025-06-17 17:27:27 +0800

    增加了readme，一个h1，一个按钮

--- test1.js
+++ test1.js
@@ -2,8 +2,8 @@ function myFunction(){
 
      // x=document.getElementById("demo");//获取ID
      // x.innerHTML="已经改变";//写入内容
-     document.getElementById("demo").innerHTML="这是javaScript函数"
-     console.log("1")
+     document.getElementById("demo").innerHTML="这是javaScript函数";
+     console.log("1");
 
 }
 
