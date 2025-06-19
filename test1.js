cc2599dc2815f363f0f81abfa2ea3bda1751ed82
TangJun <3040045453@qq.com>
2025-06-19 16:29:54 +0800

    更新了html和js里的内容

--- test1.js
+++ test1.js
@@ -1,10 +1,16 @@
-function myFunction(){
-
+function myFunction_span_1(){
      // x=document.getElementById("demo");//获取ID
      // x.innerHTML="已经改变";//写入内容
-     document.getElementById("demo").innerHTML="这是javaScript函数";
+     document.getElementById("span_1").innerHTML="span_1,这是javaScript函数";
      console.log("1");
 
 }
+
+function myFunction_h1_1(){
+     document.getElementById("h1_1").innerHTML="h1_1,这是javaScript函数";
+     console.log("1");
+
+}
+
 
 
